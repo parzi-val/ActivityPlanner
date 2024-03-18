9a189d5b109c187d65f27bfec9172bedc9ce54e6
arvind <achurox1121@gmail.com>
2024-03-19 02:06:51 +0530

    works

--- backend/views.py
+++ backend/views.py
@@ -15,9 +15,11 @@ class LLM(APIView):
 
         data = request.data
         print(data.get("text"))
-        pre = model.breakdown(data.get("text"))["args"]["params"]
-        processed_data = model.gensets(pre["budget"],pre["duration"])
-        res_data = {"activities":processed_data}
-        print(json.dumps(processed_data))
+        # pre = model.breakdown(data.get("text"))["args"]["params"]
+        # processed_data = model.gensets(pre["budget"],pre["duration"])
+        # res_data = {"activities":processed_data}
+        # print(json.dumps(processed_data))
+        res_data = {"activities": [{"title": "Place 39", "categoryName": "Swimming Pool", "address": "X5F6+Q88, VIT University, Vellore, Tamil Nadu 632014", "phone":9048131066, "url": "https://maps.app.goo.gl/8oQwfYJusgm9mhhQ8", "price": 300, "time": 2}]
+}
         return Response(json.dumps(res_data), status=status.HTTP_200_OK)
 
